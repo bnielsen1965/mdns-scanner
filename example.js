99b593c1c2139a7306f241aa24e4b88868edbd25
burnin <burnin@localhost.localdomain>
2019-10-08 07:10:48 -0700

    Refactor into distinct modules.

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
 
 const MDNSScanner = require('./lib/mdns-scanner');
+const MDNSServices = require('./lib/mdns-services');
+
+let serviceTypes = [];
 
 let scanner = new MDNSScanner({
   noInit: true,
@@ -7,7 +10,10 @@ let scanner = new MDNSScanner({
 //  interfaces: ['enp6s0']//, '192.168.18.130']
 });
 
-scanner
+let services = new MDNSServices(scanner);
+
+//scanner
+services
   .on('error', error => {
     console.log('ERROR', error.message);
   })
@@ -21,12 +27,27 @@ scanner
     console.log('QUERY', message)
   })
   .on('response', message => {
-    console.log('RESPONSE', message)
+//    console.log('RESPONSE', message)
+//    processResponse(message);
+  })
+  .on('discovered', message => {
+    console.log('DISCOVERED', message);
+    /*
+    switch (message.type) {
+      case 'type':
+        let types = services.serviceTypes.slice();
+        types.sort();
+//        console.log('STYPES', types);
+        scanner.query(message.data, 'ANY');
+        break;
+    }
+*/
   });
+
 
 scanner.init()
   .then(() => {
-    scanner.query('_services._dns-sd._udp.local', 'PTR');// { questions: [{ name: name, type: 'PTR' }] });
+    scanner.query('_services._dns-sd._udp.local', 'ANY');// 'PTR');// { questions: [{ name: name, type: 'PTR' }] });
   })
   .catch((error) => {
     console.log('ERROR', error);
@@ -35,4 +56,36 @@ scanner.init()
 
 
 
-setTimeout(function () { process.exit(0); }, 15000);
+setTimeout(function () {
+  let types = services.types.slice();
+  types.sort();
+  console.log('STYPES', types);
+  console.log(services.namedServices);
+  process.exit(0);
+}, 15000);
+
+function processResponse (response) {
+  if (!response.answers) {
+    return;
+  }
+  response.answers.forEach((answer) => {
+    switch (answer.type) {
+      case 'PTR':
+        processPTR(answer);
+        break;
+
+      default:
+        console.log('UNKNOWN TYPE', answer.type);
+        break;
+    }
+  });
+}
+
+function processPTR (answer) {
+  let serviceType = answer.data.toString();
+  if (!serviceTypes.includes(serviceType)) {
+    serviceTypes.push(serviceType);
+    serviceTypes.sort();
+    console.log('STYPES', serviceTypes);
+  }
+}
